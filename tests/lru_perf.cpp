5484a40371fbc2a674a68edfee431e49c8135306
Alexander Stavonin <alex@sysdev.me>
2019-12-09 21:15:12 +0800

    Performance tests improvements

--- tests/lru_perf.cpp
+++ tests/lru_perf.cpp
@@ -11,21 +11,67 @@ using namespace cachew;
 
 TEST_CASE( "LRU cache benchmark", "[benchmark]" )
 {
-    SECTION( "POD types" )
+    SECTION( "POD types, 500 elements" )
     {
-        lru_cache<int, int> cache( 5000 );
+        const size_t        cache_size      = 500;
+        const size_t        iteration_count = 10'000;
+        lru_cache<int, int> cache( cache_size );
 
-        BENCHMARK( "integer cache put" )
+        BENCHMARK( "integer cache put (500, 10'000 iterations)" )
         {
-            for( int i = 0; i < 100000; i++ )
+            for( size_t i = 0; i < iteration_count; i++ )
             {
-                cache.put( i, i * 10 );
+                cache.put( i, i );
             }
         };
 
-        BENCHMARK( "integer cache get" )
+        BENCHMARK( "integer cache get (10'000 iterations)" )
         {
-            for( int i = 0; i < 100000; i++ )
+            for( size_t i = 0; i < iteration_count; i++ )
+            {
+                cache.get( i );
+            }
+        };
+    }
+    SECTION( "POD types, 5000 elements" )
+    {
+        const size_t        cache_size      = 5000;
+        const size_t        iteration_count = 10'000;
+        lru_cache<int, int> cache( cache_size );
+
+        BENCHMARK( "integer cache put (5000, 10'000 iterations)" )
+        {
+            for( size_t i = 0; i < iteration_count; i++ )
+            {
+                cache.put( i, i );
+            }
+        };
+
+        BENCHMARK( "integer cache get (10'000 iterations)" )
+        {
+            for( size_t i = 0; i < iteration_count; i++ )
+            {
+                cache.get( i );
+            }
+        };
+    }
+    SECTION( "POD types, 50'000 elements" )
+    {
+        const size_t        cache_size      = 50'000;
+        const size_t        iteration_count = 100'000;
+        lru_cache<int, int> cache( cache_size );
+
+        BENCHMARK( "integer cache put (50'000, 100'000 iterations)" )
+        {
+            for( size_t i = 0; i < iteration_count; i++ )
+            {
+                cache.put( i, i );
+            }
+        };
+
+        BENCHMARK( "integer cache get (100'000 iterations)" )
+        {
+            for( size_t i = 0; i < iteration_count; i++ )
             {
                 cache.get( i );
             }
